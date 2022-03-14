b3e0394b94a1af5d39d6337bc0c60de272ceed9d
shangmingyu <shangmingyu@baidu.com>
2022-03-14 19:42:54 +0800

    quick sort

---
+++ design/principles/SOLID.go
@@ -0,0 +1,7 @@
+package principles
+
+// 1、单一职责原则的英文是 Single Responsibility Principle，缩写为 SRP。
+//		A class or module should have a single responsibility。
+//		翻译成中文：一个类或者模块只负责完成一个职责（或者功能）。
+// 	 理解：一个类只负责完成一个职责或者功能。也就是说，不要设计大而全的类，要设计粒度小、功能单一的类。
+//		换个角度来讲就是，一个类包含了两个或者两个以上业务不相干的功能，那我们就说它职责不够单一，应该将它拆分成多个功能更加单一、粒度更细的类。
