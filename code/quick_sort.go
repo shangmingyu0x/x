b6969c533fd15d8dd6e925bc672017f4db476c7f
shangmingyu <mingyu_shang@shannonai.com>
2022-07-17 21:16:12 +0800

    modify quick sort

--- code/quick_sort.go
+++ code/quick_sort.go
@@ -1,27 +1,23 @@
 package code
 
-func quickSort(nums *[]int, start, end int) {
+func quickSort(nums []int, start, end int) {
 	if start >= end {
 		return
 	}
 
+	pivot := nums[(start+end)/2]
 	left, right := start, end
-	// 1. pivot，nums[start], nums[end]: get value not index
-	pivot := (*nums)[(start+end)/2]
-	// 2. left <= right not left < right
 	for left <= right {
-		// 3. nums[left] < pivot  not <=
-		for left <= right && (*nums)[left] < pivot {
+		for left <= right && nums[left] < pivot {
 			left++
 		}
 
-		for left <= right && (*nums)[right] > pivot {
+		for left <= right && nums[right] > pivot {
 			right--
 		}
 
 		if left <= right {
-			(*nums)[left], (*nums)[right] = (*nums)[right], (*nums)[left]
-			// 4. don't forget left++ right--
+			nums[left], nums[right] = nums[right], nums[left]
 			left++
 			right--
 		}
