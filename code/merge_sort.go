c67883000d4e0ee74740fd4cb1e11301199b4d93
shangmingyu <mingyu_shang@shannonai.com>
2022-07-17 21:05:03 +0800

    add merge sort

---
+++ code/merge_sort.go
@@ -0,0 +1,51 @@
+package code
+
+// mergeSort .
+func mergeSort(nums []int) []int {
+
+	n := len(nums)
+	if n <= 1 {
+		return nums
+	}
+
+	mid := n / 2
+	left := mergeSort(nums[:mid])
+	right := mergeSort(nums[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	var ans []int
+
+	ls, rs := 0, 0
+	le, re := len(left)-1, len(right)-1
+	for ls <= le && rs <= re {
+		if left[ls] < right[rs] {
+			ans = append(ans, left[ls])
+			ls++
+		} else {
+			ans = append(ans, right[rs])
+			rs++
+		}
+	}
+
+	for ls <= le {
+		ans = append(ans, left[ls])
+		ls++
+	}
+
+	for rs <= re {
+		ans = append(ans, right[rs])
+		rs++
+	}
+
+	// if ls <= le {
+	// 	ans = append(ans, left[ls:]...)
+	// }
+	// if rs <= re {
+	// 	ans = append(ans, right[rs:]...)
+	// }
+
+	return ans
+}
